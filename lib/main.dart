f9089e099c15356e3588e5bfa77f3c248d3d0c1b
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-06-28 20:56:22 +0500

    1st Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bmi_calculator/screens/input_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
